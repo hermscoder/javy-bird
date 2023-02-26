5174e3a318a700433bb9291bac2f4baee4f66aa3
Emerson Ribeiro <emersonrjr03@gmail.com>
2023-02-26 21:55:59 +0000

    #2 Adding inputs to be displayed on the screen

--- src/main/java/com/emersonribeiro/gamestate/Playing.java
+++ src/main/java/com/emersonribeiro/gamestate/Playing.java
@@ -3,12 +3,15 @@ package com.emersonribeiro.gamestate;
 
 import com.emersonribeiro.main.Game;
 
-import java.awt.*;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 
-public class Playing extends State implements StateMethods{
+public class Playing extends State implements StateMethods {
+
+    private StringBuilder keyPressedBuffer;
+    private boolean clearBuffer;
 
     public Playing(Game game) {
         super(game);
@@ -16,21 +19,27 @@ public class Playing extends State implements StateMethods{
     }
 
     private void initClasses() {
+        keyPressedBuffer = new StringBuilder();
     }
 
 
     public void update() {
+        if (clearBuffer) {
+            keyPressedBuffer = new StringBuilder();
+            clearBuffer = false;
+        }
         //TODO Update playing related stuff, obstacles, player, etc
     }
 
 
     public void draw(Graphics g) {
         //TODO Draw playing related stuff, obstacles, player, etc
+        g.drawString(keyPressedBuffer.toString(), 20, 20);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-
+        keyPressedBuffer.append(" click ");
     }
 
     @Override
@@ -50,7 +59,14 @@ public class Playing extends State implements StateMethods{
 
     @Override
     public void keyPressed(KeyEvent e) {
+        keyPressedBuffer
+                .append(" ")
+                .append(e.getKeyChar())
+                .append(" ");
 
+        if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+            clearBuffer = true;
+        }
     }
 
     @Override
