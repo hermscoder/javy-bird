1c757bbd3992416228df868418bbeaa0dc4d19df
Emerson Ribeiro <emersonrjr03@gmail.com>
2023-03-03 07:13:59 +0000

    #2 Drawing better floor on level render, adding object manager and columns

--- src/main/java/com/emersonribeiro/level/LevelRender.java
+++ src/main/java/com/emersonribeiro/level/LevelRender.java
@@ -2,18 +2,25 @@ package com.emersonribeiro.level;
 
 import com.emersonribeiro.main.Game;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class LevelRender {
 
-    private static final Color FLOOR_COLOR = new Color(192,239,204);
+    public static final int FLOOR_HEIGHT = 120;
+    private static final int GRASS_HEIGHT = 20;
+    private static final int UNDERGROUND_HEIGHT = 100;
+
     public void update() {
         //TODO update level related stuff
     }
 
     public void draw(Graphics g) {
-        g.setColor(FLOOR_COLOR);
-        g.fillRect(0, Game.GAME_HEIGHT - 20, Game.GAME_WIDTH, 20);
-
+        // grass
+        g.setColor(new Color(192, 239, 204));
+        g.fillRect(0, Game.GAME_HEIGHT - FLOOR_HEIGHT, Game.GAME_WIDTH, GRASS_HEIGHT);
+        // underground
+        g.setColor(new Color(217, 188, 154));
+        g.fillRect(0, Game.GAME_HEIGHT - UNDERGROUND_HEIGHT, Game.GAME_WIDTH, UNDERGROUND_HEIGHT);
     }
 }
